79ce5ed45da7304bb7e012ac60778ccded39f6fb
Aldair Lara <aldairlara@gmail.com>
2025-09-01 11:57:42 -0600

    Add 'c10'

---
+++ Workspace/c10-ProgramaFuente/main.asm
@@ -0,0 +1,31 @@
+; *************************************************************************************************
+; Universidad Nacional Autónoma de México (UNAM)
+; Facultad de Ingeniería | Departamento de Electrónica
+; 
+; Asignatura:   Microprocesadores y Microcontroladores
+; Profesor:     M.I. Christo Aldair Lara Tenorio
+; Fecha:        31 de agosto de 2025
+; 
+; Tema 04:      Lenguaje ensamblador y el ensamblador
+; Código 10:    Programa fuente vs programa objeto
+; Descripción:  Comparación entre el programa fuente y el programa objeto.
+; 
+; Tarjeta de desarrollo:        EK-TM4C1294XL Evaluation board
+; ***********************************************
+
+        .global main
+
+
+; *************************************************************************************************
+; Sección de código ejecutable
+; ***********************************************
+
+main:
+
+        MOVW  R0, #0x0004
+        MOVT  R0, #0x2000                       ; R0 = 0x2000.0004
+loop    LDR   R1, [R0], #4                      ; R1 = [R0], R0 = R0 + 4
+        ADD   R2, R1                            ; R2 = R2 + R1
+        B     loop                              ; Saltar a loop
+
+        .end
