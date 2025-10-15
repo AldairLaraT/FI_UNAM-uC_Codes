9ae70d49244e1028ed78ec6b4d5612aba2e8b2c0
Aldair Lara <aldairlara@gmail.com>
2025-10-15 15:05:36 -0600

    Add 'c33'

---
+++ Workspace/c33-UserSWs/main.c
@@ -0,0 +1,113 @@
+/**************************************************************************************************
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ * 
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       15 de octubre de 2025
+ * 
+ * Tema 08:     Lenguaje C
+ * Código 33:   Control de un LED con un SW de usuario
+ * Descripción: Código en lenguaje C que enciende/apaga el LED D1 de la tarjeta de desarrollo,
+ *              dependiendo del botón SW1.
+ * 
+ * Tarjeta de desarrollo:       EK-TM4C1294XL Evaluation board
+ ***********************************************/
+
+
+/**************************************************************************************************
+ * Archivos de cabecera
+ */
+
+#include <stdint.h>                                                                                 /*  Tipos enteros con tamaños fijos */
+
+
+/**************************************************************************************************
+ * Definiciones y macros
+ */
+
+/************************************************
+ * System Control (SYSCTL) registers                                                                    pp247   Register map
+ */
+#define SYSCTL_RCGCGPIO_R           (*((volatile uint32_t *)0x400FE608))                            /*  pp382   GPIO Run Mode Clock Gating Control */
+#define SYSCTL_PRGPIO_R             (*((volatile uint32_t *)0x400FEA08))                            /*  pp499   GPIO Peripheral Ready */
+
+/************************************************
+ * General-Purpose Input/Output (GPIO) registers                                                        pp757   Register map
+ */
+    // GPIO Port J (PortJ)
+#define GPIO_PORTJ_AHB_DATA_R       (*((volatile uint32_t *)0x40060004))                            /*  pp759   GPIO Data >> PortJ[0] unmasked */
+#define GPIO_PORTJ_AHB_DIR_R        (*((volatile uint32_t *)0x40060400))                            /*  pp760   GPIO Direction */
+#define GPIO_PORTJ_AHB_PUR_R        (*((volatile uint32_t *)0x40060510))                            /*  pp776   GPIO Pull-Up Select */
+#define GPIO_PORTJ_AHB_DEN_R        (*((volatile uint32_t *)0x4006051C))                            /*  pp781   GPIO Digital Enable */
+    // GPIO Port N (PortN)
+#define GPIO_PORTN_DATA_R           (*((volatile uint32_t *)0x40064008))                            /*  pp759   GPIO Data >> PortN[1] unmasked */
+#define GPIO_PORTN_DIR_R            (*((volatile uint32_t *)0x40064400))                            /*  pp760   GPIO Direction */
+#define GPIO_PORTN_DEN_R            (*((volatile uint32_t *)0x4006451C))                            /*  pp781   GPIO Digital Enable */
+
+
+/**************************************************************************************************
+ * Funciones
+ */
+
+ /************************************************
+ * Función:     GPIO_PortJ_Init
+ * 
+ * Descripción: Inicialización y configuración del puerto GPIO J.
+ */
+
+void GPIO_PortJ_Init(void) {
+
+    /*  1. Habilitar la señal de reloj del GPIO y esperar a que se estabilice el reloj. */
+    SYSCTL_RCGCGPIO_R |= 0x0100;                                                                    /*  R8: GPIO PortJ Run Mode Clock Gating Control -> Enabled */
+    while (!(SYSCTL_PRGPIO_R & 0x0100)) {}                                                          /*  R8: GPIO PortJ Peripheral Ready -> GPIO PortJ is ready for access */
+
+    /*  2. Configurar la dirección de los pines del puerto GPIO. */
+    GPIO_PORTJ_AHB_DIR_R &= ~0x01;                                                                  /*  PortJ[0] => DIR: GPIO Data direction -> Input */
+
+    /*  3. Habilitar las resistencias de pull-up de los pines del puerto GPIO. */
+    GPIO_PORTJ_AHB_PUR_R |= 0x01;                                                                   /*  PortJ[0] => PUE: Pad Weak Pull-Up Enable -> Enabled */
+
+    /*  4. Habilitar las funciones digitales de los pines del puerto GPIO. */
+    GPIO_PORTJ_AHB_DEN_R |= 0x01;                                                                   /*  PortJ[0] => DEN: Digital Enable -> Enabled */
+
+}
+
+/************************************************
+ * Función:     GPIO_PortN_Init
+ * 
+ * Descripción: Inicialización y configuración del puerto GPIO N.
+ */
+
+void GPIO_PortN_Init(void) {
+
+    /*  1. Habilitar la señal de reloj del GPIO y esperar a que se estabilice el reloj. */
+    SYSCTL_RCGCGPIO_R |= 0x1000;                                                                    /*  R12: GPIO PortN Run Mode Clock Gating Control -> Enabled */
+    while (!(SYSCTL_PRGPIO_R & 0x1000)) {}                                                          /*  R12: GPIO PortN Peripheral Ready -> GPIO PortN is ready for access */
+
+    /*  2. Configurar la dirección de los pines del puerto GPIO. */
+    GPIO_PORTN_DIR_R |= 0x02;                                                                       /*  PortN[1] => DIR: GPIO Data direction -> Output */
+
+    /*  3. Habilitar las funciones digitales de los pines del puerto GPIO. */
+    GPIO_PORTN_DEN_R |= 0x02;                                                                       /*  PortN[1] => DEN: Digital Enable -> Enabled */
+
+}
+
+
+/**************************************************************************************************
+ * Función principal
+ */
+
+int main(void) {
+
+    GPIO_PortJ_Init();                                                                              /*  Inicialización y configuración del puerto GPIO J */
+    GPIO_PortN_Init();                                                                              /*  Inicialización y configuración del puerto GPIO N */
+
+    while (1) {
+        if (GPIO_PORTJ_AHB_DATA_R == 0x00) {                                                        /*  IF (SW1 -> on) */
+            GPIO_PORTN_DATA_R |= 0x02;                                                              /*  LED D1 -> on */
+        } else {                                                                                    /*  IF (SW1 -> off) */
+            GPIO_PORTN_DATA_R &= ~0x02;                                                             /*  LED D1 -> off */
+        }
+    }
+}
