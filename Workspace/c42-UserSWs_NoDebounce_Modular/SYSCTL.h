fcbf9ecf0910d4c30fd84c5f2c8b5a6ad8ab94ca
Aldair Lara <aldairlara@gmail.com>
2025-11-02 15:48:27 -0600

    Add 'c42'

---
+++ Workspace/c42-UserSWs_NoDebounce_Modular/SYSCTL.h
@@ -0,0 +1,44 @@
+/**************************************************************************************************
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ * 
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       01 de noviembre de 2025
+ * 
+ * Tema 09:     Periféricos
+ * Código 42:   Control de un LED con un SW de usuario sin debounce (modular)
+ * Descripción: Código en lenguaje C que conmuta el LED D1 de la tarjeta de desarrollo,
+ *              dependiendo del botón SW1, sin considerar el efecto rebote.
+ * 
+ * Archivo:     Archivo de cabecera del módulo SYSCTL
+ * 
+ * Tarjeta de desarrollo:       EK-TM4C1294XL Evaluation board
+ ***********************************************/
+
+
+#ifndef SYSCTL_H                                                                                    /*  Verificar si SYSCTL_H no ha sido definido previamente */
+#define SYSCTL_H                                                                                    /*  Definir SYSCTL_H para evitar inclusiones múltiples del mismo archivo */
+
+
+/**************************************************************************************************
+ * Archivos de cabecera
+ */
+
+#include <stdint.h>                                                                                 /*  Tipos enteros con tamaños fijos */
+
+
+/**************************************************************************************************
+ * Definiciones y macros
+ * System Control (SYSCTL) registers                                                                    pp247   Register map
+ */
+#define SYSCTL_RCGCGPIO_R           (*((volatile uint32_t *)0x400FE608))                            /*  pp382   GPIO Run Mode Clock Gating Control */
+#define SYSCTL_PRGPIO_R             (*((volatile uint32_t *)0x400FEA08))                            /*  pp499   GPIO Peripheral Ready */
+
+
+/**************************************************************************************************
+ * Prototipos de funciones
+ */
+
+
+#endif                                                                                              /*  SYSCTL_H */
---
+++ Workspace/c42-UserSWs_NoDebounce_Modular/main.c
@@ -0,0 +1,46 @@
+/**************************************************************************************************
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ * 
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       01 de noviembre de 2025
+ * 
+ * Tema 09:     Periféricos
+ * Código 42:   Control de un LED con un SW de usuario sin debounce (modular)
+ * Descripción: Código en lenguaje C que conmuta el LED D1 de la tarjeta de desarrollo,
+ *              dependiendo del botón SW1, sin considerar el efecto rebote.
+ * 
+ * Tarjeta de desarrollo:       EK-TM4C1294XL Evaluation board
+ ***********************************************/
+
+
+/**************************************************************************************************
+ * Archivos de cabecera
+ */
+
+#include <stdint.h>                                                                                 /*  Tipos enteros con tamaños fijos */
+#include "GPIO.h"                                                                                   /*  Archivo de cabecera del módulo GPIO */
+#include "SYSCTL.h"                                                                                 /*  Archivo de cabecera del módulo SYSCTL */
+
+
+/**************************************************************************************************
+ * Variables globales
+ */
+
+
+/**************************************************************************************************
+ * Función principal
+ */
+
+int main(void) {
+
+    GPIO_PortJ_Init();                                                                              /*  Inicialización y configuración del puerto GPIO J */
+    GPIO_PortN_Init();                                                                              /*  Inicialización y configuración del puerto GPIO N */
+
+    while (1) {
+        if (GPIO_PORTJ_AHB_DATA_R == 0x00) {                                                        /*  IF (SW1 -> on) */
+            GPIO_PORTN_DATA_R ^= 0x02;                                                              /*  LED D1 -> toggle */
+        }
+    }
+}
