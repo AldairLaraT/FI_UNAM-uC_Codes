5eb33d409c6e495eb62b91dc802d0772b5a5fea2
Aldair Lara <aldairlara@gmail.com>
2025-11-02 16:40:08 -0600

    Add 'c45'

---
+++ Workspace/c45-SysTick_OneShot_Modular/GPIO.c
@@ -0,0 +1,50 @@
+/**************************************************************************************************
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ * 
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       01 de noviembre de 2025
+ * 
+ * Tema 09:     Periféricos
+ * Código 45:   SysTick en modo one-shot (modular)
+ * Descripción: Código en lenguaje C que conmuta el LED D1 de la tarjeta de desarrollo con una
+ *              frecuencia de 1Hz, empleando el SysTick en modo one-shot.
+ * 
+ * Archivo:     Archivo fuente del módulo GPIO
+ * 
+ * Tarjeta de desarrollo:       EK-TM4C1294XL Evaluation board
+ ***********************************************/
+
+
+/**************************************************************************************************
+ * Archivos de cabecera
+ */
+
+#include "GPIO.h"                                                                                   /*  Archivo de cabecera del módulo GPIO */
+#include "SYSCTL.h"                                                                                 /*  Archivo de cabecera del módulo SYSCTL */
+
+
+/**************************************************************************************************
+ * Funciones
+ */
+
+/************************************************
+ * Función:     GPIO_PortN_Init
+ * 
+ * Descripción: Inicialización y configuración del puerto GPIO N.
+ */
+
+void GPIO_PortN_Init(void) {
+
+    /*  1. Habilitar la señal de reloj del GPIO y esperar a que se estabilice el reloj. */
+    SYSCTL_RCGCGPIO_R |= 0x1000;                                                                    /*  R12: GPIO PortN Run Mode Clock Gating Control -> Enabled */
+    while (!(SYSCTL_PRGPIO_R & 0x1000)) {}                                                          /*  R12: GPIO PortN Peripheral Ready -> GPIO PortN is ready for access */
+
+    /*  2. Configurar la dirección de los pines del puerto GPIO. */
+    GPIO_PORTN_DIR_R |= 0x02;                                                                       /*  PortN[1] => DIR: GPIO Data direction -> Output */
+
+    /*  3. Habilitar las funciones digitales de los pines del puerto GPIO. */
+    GPIO_PORTN_DEN_R |= 0x02;                                                                       /*  PortN[1] => DEN: Digital Enable -> Enabled */
+
+}
---
+++ Workspace/c45-SysTick_OneShot_Modular/GPIO.h
@@ -0,0 +1,48 @@
+/**************************************************************************************************
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ * 
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       01 de noviembre de 2025
+ * 
+ * Tema 09:     Periféricos
+ * Código 45:   SysTick en modo one-shot (modular)
+ * Descripción: Código en lenguaje C que conmuta el LED D1 de la tarjeta de desarrollo con una
+ *              frecuencia de 1Hz, empleando el SysTick en modo one-shot.
+ * 
+ * Archivo:     Archivo de cabecera del módulo GPIO
+ * 
+ * Tarjeta de desarrollo:       EK-TM4C1294XL Evaluation board
+ ***********************************************/
+
+
+#ifndef GPIO_H                                                                                      /*  Verificar si GPIO_H no ha sido definido previamente */
+#define GPIO_H                                                                                      /*  Definir GPIO_H para evitar inclusiones múltiples del mismo archivo */
+
+
+/**************************************************************************************************
+ * Archivos de cabecera
+ */
+
+#include <stdint.h>                                                                                 /*  Tipos enteros con tamaños fijos */
+
+
+/**************************************************************************************************
+ * Definiciones y macros
+ * General-Purpose Input/Output (GPIO) registers                                                        pp757   Register map
+ */
+    /*  GPIO Port N (PortN) */
+#define GPIO_PORTN_DATA_R           (*((volatile uint32_t *)0x40064008))                            /*  pp759   GPIO Data >> PortN[1] unmasked */
+#define GPIO_PORTN_DIR_R            (*((volatile uint32_t *)0x40064400))                            /*  pp760   GPIO Direction */
+#define GPIO_PORTN_DEN_R            (*((volatile uint32_t *)0x4006451C))                            /*  pp781   GPIO Digital Enable */
+
+
+/**************************************************************************************************
+ * Prototipos de funciones
+ */
+
+void GPIO_PortN_Init();                                                                             /*  Inicialización y configuración del puerto GPIO N */
+
+
+#endif                                                                                              /*  GPIO_H */
