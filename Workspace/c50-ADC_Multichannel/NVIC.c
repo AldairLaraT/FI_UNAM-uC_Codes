b95b19d00cf893f8af748ff66cb6749c8a3576d3
Aldair Lara <aldairlara@gmail.com>
2025-11-12 16:38:22 -0600

    Update comments

--- Workspace/c50-ADC_Multichannel/NVIC.c
+++ Workspace/c50-ADC_Multichannel/NVIC.c
@@ -1,18 +1,18 @@
 /**************************************************************************************************
- * Universidad Nacional Autónoma de México (UNAM)
- * Facultad de Ingeniería | Departamento de Electrónica
+ *  Universidad Nacional Autónoma de México (UNAM)
+ *  Facultad de Ingeniería | Departamento de Electrónica
  * 
- * Asignatura:  Microprocesadores y Microcontroladores
- * Profesor:    M.I. Christo Aldair Lara Tenorio
- * Fecha:       06 de noviembre de 2025
+ *  Asignatura:     Microprocesadores y Microcontroladores
+ *  Profesor:       M.I. Christo Aldair Lara Tenorio
+ *  Fecha:          06 de noviembre de 2025
  * 
- * Tema 09:     Periféricos
- * Código 50:   Convertidor analógico digital (ADC): Multicanal
- * Descripción: Código en lenguaje C que configura el módulo ADC_0, empleando el secuenciador de
- *              muestras SS_1 para leer el valor de dos potenciómetros conectados en las entradas
- *              analógicas AIN_10 (PB4) y AIN_17 (PK1), usando los LED de usuario de la tarjeta
- *              de desarrollo como indicador del nivel de voltaje de un potenciómetro, dependiendo
- *              del último botón (SW1 o SW2) presionado.
+ *  Tema 09:        Periféricos
+ *  Código 50:      Convertidor analógico digital (ADC): Multicanal
+ *  Descripción:    Código en lenguaje C que configura el módulo ADC_0, empleando el secuenciador
+ *                  de muestras SS_1 para leer el valor de dos potenciómetros conectados en las
+ *                  entradas analógicas AIN_10 (PB4) y AIN_17 (PK1), usando los LED de usuario de
+ *                  la tarjeta de desarrollo como indicador del nivel de voltaje de un
+ *                  potenciómetro, dependiendo del último botón (SW1 o SW2) presionado.
  * 
  *  Archivo:        Archivo fuente del módulo NVIC
  * 
