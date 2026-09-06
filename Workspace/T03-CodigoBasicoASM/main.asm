356d2385b2a097c43dd744bafa9f4c7333aa944f
Aldair Lara <aldairlarat@gmail.com>
2026-09-06 15:15:30 -0600

    feat: update to 2027-1 version

--- Workspace/T03-CodigoBasicoASM/main.asm
+++ Workspace/T03-CodigoBasicoASM/main.asm
@@ -1,28 +1,29 @@
-; *************************************************************************************************
+; -------------------------------------------------------------------------------------------------
 ; Universidad Nacional Autónoma de México (UNAM)
 ; Facultad de Ingeniería | Departamento de Electrónica
-; 
+;
 ; Asignatura:   Microprocesadores y Microcontroladores
 ; Profesor:     M.I. Christo Aldair Lara Tenorio
 ; Fecha:        24 de agosto de 2025
-; 
+;
 ; Tema 03:      Modos de direccionamiento y conjunto de instrucciones
 ; Tarea 03:     Código básico en lenguaje ensamblador
 ; Descripción:  Proyecto vacío para realizar los ejercicios de la tarea 3.
-; 
+;
 ; Tarjeta de desarrollo:        EK-TM4C1294XL Evaluation board
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 
         .global main
 
 
-; *************************************************************************************************
+; -------------------------------------------------------------------------------------------------
 ; Sección de código ejecutable
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 
 main:
 
 
 
-end     B     end
+halt    B       halt
+
         .end
