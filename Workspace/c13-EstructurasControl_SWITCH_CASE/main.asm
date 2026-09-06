d8da193d81b9beeac7502c81669af4966e80ced0
Aldair Lara <aldairlarat@gmail.com>
2026-09-05 19:14:48 -0600

    feat: update to 2027-1 version

--- Workspace/c13-EstructurasControl_SWITCH_CASE/main.asm
+++ Workspace/c13-EstructurasControl_SWITCH_CASE/main.asm
@@ -1,63 +1,67 @@
-; *************************************************************************************************
+; -------------------------------------------------------------------------------------------------
 ; Universidad Nacional Autónoma de México (UNAM)
 ; Facultad de Ingeniería | Departamento de Electrónica
-; 
+;
 ; Asignatura:   Microprocesadores y Microcontroladores
 ; Profesor:     M.I. Christo Aldair Lara Tenorio
 ; Fecha:        17 de septiembre de 2025
-; 
+;
 ; Tema 05:      Programación estructurada en lenguaje ensamblador
-; Código 14:    Estructuras de control: SWITCH-CASE
+; Código 13:    Estructuras de control: SWITCH-CASE
 ; Descripción:  Construcción de la estructura de control SWITCH-CASE en lenguaje ensamblador.
-;               Selección de un bloque de instrucciones entre varias opciones, a partir del valor de una variable.
-; 
+;               Selección de un bloque de instrucciones entre varias opciones, a partir del valor
+;               de una variable.
+;
 ; Tarjeta de desarrollo:        EK-TM4C1294XL Evaluation board
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 
         .global main
 
 
-; *************************************************************************************************
+; -------------------------------------------------------------------------------------------------
 ; Sección de datos
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 
         .data
 
 
-; *************************************************************************************************
+; -------------------------------------------------------------------------------------------------
 ; Sección de código ejecutable
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 
         .text
 
 
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 ; Apuntadores
 
 
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 ; Subrutinas
 
 
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 ; Código principal
 
 main:
-        CMP   R0, #1                            ; Condición del case1 (R0 = 1)
-        BEQ   case1
-        CMP   R0, #2                            ; Condición del case2 (R0 = 2)
-        BEQ   case2
-        B     case_default
+
+        CMP     R0, #1
+        BEQ     case1                           ; Condición del case1 (R0 = 1)
+
+        CMP     R0, #2
+        BEQ     case2                           ; Condición del case2 (R0 = 2)
+
+        B       case_default
 
 case1                                           ; Bloque case1
         NOP
         NOP
-        B     switch_end
+        B       switch_end
 
 case2                                           ; Bloque case2
         NOP
         NOP
-        B     switch_end
+        B       switch_end
 
 case_default                                    ; Bloque DEFAULT
         NOP
@@ -67,5 +71,7 @@ switch_end                                      ; Fin del bloque CASE
         NOP
         NOP
 
-end     B     end
+
+halt    B       halt
+
         .end
