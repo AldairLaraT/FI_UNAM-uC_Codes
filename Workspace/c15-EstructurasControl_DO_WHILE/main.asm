cf89fcd1d59fd3f654e706f25549a79a4b7ac43e
Aldair Lara <aldairlarat@gmail.com>
2026-09-05 19:41:19 -0600

    feat: update to 2027-1 version

--- Workspace/c15-EstructurasControl_DO_WHILE/main.asm
+++ Workspace/c15-EstructurasControl_DO_WHILE/main.asm
@@ -1,59 +1,66 @@
-; *************************************************************************************************
+; -------------------------------------------------------------------------------------------------
 ; Universidad Nacional Autónoma de México (UNAM)
 ; Facultad de Ingeniería | Departamento de Electrónica
-; 
+;
 ; Asignatura:   Microprocesadores y Microcontroladores
 ; Profesor:     M.I. Christo Aldair Lara Tenorio
 ; Fecha:        17 de septiembre de 2025
-; 
+;
 ; Tema 05:      Programación estructurada en lenguaje ensamblador
-; Código 16:    Estructuras de control: DO-WHILE
+; Código 15:    Estructuras de control: DO-WHILE
 ; Descripción:  Construcción de la estructura de control DO-WHILE en lenguaje ensamblador.
-;               Ejecución de un bloque de instrucciones al menos una vez y repetición si se cumple una condición.
-; 
+;               Ejecución de un bloque de instrucciones al menos una vez y repetición si se cumple
+;               una condición.
+;
 ; Tarjeta de desarrollo:        EK-TM4C1294XL Evaluation board
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 
         .global main
 
 
-; *************************************************************************************************
+; -------------------------------------------------------------------------------------------------
 ; Sección de datos
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 
         .data
 
 
-; *************************************************************************************************
+; -------------------------------------------------------------------------------------------------
 ; Sección de código ejecutable
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 
         .text
 
 
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 ; Apuntadores
 
 
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 ; Subrutinas
 
 
-; ***********************************************
+; -------------------------------------------------------------------------------------------------
 ; Código principal
 
 main:
+
         NOP
         NOP
 
 do_while_block                                  ; Bloque DO-WHILE
         NOP
         NOP
-        CMP   R0, #5                            ; Condición (R0 < 5)
-        BMI   do_while_block
 
-        NOP                                     ; Fin del bloque DO-WHILE
+    ; Condición (R0 < 5)
+        CMP     R0, #5
+        BMI     do_while_block
+
+do_while_end                                    ; Fin del bloque DO-WHILE
+        NOP
         NOP
 
-end     B     end
+
+halt    B       halt
+
         .end
