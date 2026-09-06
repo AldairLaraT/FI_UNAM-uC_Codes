f6633b7bba434fef05730508d798365c77896ede
Aldair Lara <aldairlarat@gmail.com>
2026-09-06 12:15:08 -0600

    feat: update to 2027-1 version

--- Workspace/c09-ProgramaFuente/main.asm
+++ Workspace/c09-ProgramaFuente/main.asm
@@ -24,10 +24,15 @@ main:
 
         MOVW    R0, #0x0004
         MOVT    R0, #0x2000                     ; R0 = 0x2000_0004
+        MOVW    R1, #0xFFFC
+        MOVT    R1, #0x2003                     ; R1 = 0x2003_FFFC
+        MOV     R3, #0                          ; R3 = 0
 
-loop    LDR     R1, [R0], #4                    ; R1 = [R0], R0 = R0 + 4
-        ADD     R2, R1                          ; R2 = R2 + R1
-        B       loop                            ; Saltar a loop
+loop    LDR     R2, [R0], #4                    ; R2 = [R0], R0 = R0 + 4
+        ADD     R3, R2                          ; R3 = R3 + R2
+
+        CMP     R0, R1                          ; Comparar R0 con R1
+        BNE     loop                            ; Saltar si (Z = 0)
 
 
 halt    B       halt
