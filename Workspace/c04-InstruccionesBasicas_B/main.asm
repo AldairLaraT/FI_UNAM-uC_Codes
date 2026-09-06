1c7af1b202ee191d04e9e34e1de9ef66bea6a3b4
Aldair Lara <aldairlarat@gmail.com>
2026-09-06 14:25:33 -0600

    feat: update to 2027-1 version

--- Workspace/c04-InstruccionesBasicas_B/main.asm
+++ Workspace/c04-InstruccionesBasicas_B/main.asm
@@ -9,6 +9,9 @@
 ; Tema 03:      Modos de direccionamiento y conjunto de instrucciones
 ; Código 04:    Instrucciones básicas del ARM Cortex-M | B
 ; Descripción:  Uso básico de la instrucción B.
+;               NOTA: El código no inicializa el contenido de la memoria, por lo que es necesario
+;                     utilizar la herramienta "Memory Browser" para observar el comportamiento de
+;                     las instrucciones.
 ;
 ; Tarjeta de desarrollo:        EK-TM4C1294XL Evaluation board
 ; -------------------------------------------------------------------------------------------------
