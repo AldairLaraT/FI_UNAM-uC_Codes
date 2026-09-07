d3d98fbdb01c383cac69f0aa4082ac79ae820ad4
Aldair Lara <aldairlarat@gmail.com>
2026-09-06 22:02:21 -0600

    feat: update to 2027-1 version

--- Workspace/c25-EstructurasControl_IF/main.c
+++ Workspace/c25-EstructurasControl_IF/main.c
@@ -1,50 +1,53 @@
-/**************************************************************************************************
- *  Universidad Nacional Autónoma de México (UNAM)
- *  Facultad de Ingeniería | Departamento de Electrónica
- * 
- *  Asignatura:     Microprocesadores y Microcontroladores
- *  Profesor:       M.I. Christo Aldair Lara Tenorio
- *  Fecha:          15 de octubre de 2025
- * 
- *  Tema 08:        Lenguaje C
- *  Código 26:      Estructuras de control: IF
- *  Descripción:    Construcción de la estructura de control IF en lenguaje C.
- *                  Ejecución de un bloque de instrucciones solo si se cumple una condición.
- * 
- *  Tarjeta de desarrollo:  EK-TM4C1294XL Evaluation board
- ***********************************************/
-
-
-/*********************************************************************************
- *  Archivos de cabecera
- */
-
-#include <stdint.h>                                                                                 /*  Tipos enteros con tamaños fijos */
-
-
-/*********************************************************************************
- *  Definiciones y macros
+/** -----------------------------------------------------------------------------------------------
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ *
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       15 de octubre de 2025
+ *
+ * Tema 08:     Lenguaje C
+ * Código 25:   Estructuras de control: IF
+ * Descripción: Construcción de la estructura de control IF en lenguaje C.
+ *              Ejecución de un bloque de instrucciones solo si se cumple una condición.
+ *
+ * Tarjeta de desarrollo:   EK-TM4C1294XL Evaluation board
  */
 
 
-/*********************************************************************************
- *  Funciones
+/** -----------------------------------------------------------------------------------------------
+ * Archivos de cabecera
+ */
+
+#include <stdint.h>                                                                                 /* Tipos enteros con tamaños fijos */
+
+
+/** -----------------------------------------------------------------------------------------------
+ * Definiciones y macros
  */
 
 
-/*********************************************************************************
- *  Función principal
+/** -----------------------------------------------------------------------------------------------
+ * Funciones
+ */
+
+
+/** -----------------------------------------------------------------------------------------------
+ * Función principal
  */
 
 int main(void) {
 
-    int i = 0;
+    int32_t i = 0;
 
-    int x = 5;                                                                                      /*  Variable de prueba (condición) */
+    int32_t x = 5;                                                                                  /* Variable de prueba (condición) */
 
     if (x == 5) {
         /** Código del bloque IF. */
         i++;
         i++;
     }
+
+    while (1) {}
+
 }
