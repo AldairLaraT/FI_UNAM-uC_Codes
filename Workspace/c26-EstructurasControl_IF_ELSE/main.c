220b1bfe94be1184c8a66d5435fcad670d770a75
Aldair Lara <aldairlarat@gmail.com>
2026-09-07 00:29:24 -0600

    feat: update to 2027-1 version

--- Workspace/c26-EstructurasControl_IF_ELSE/main.c
+++ Workspace/c26-EstructurasControl_IF_ELSE/main.c
@@ -1,48 +1,48 @@
-/**************************************************************************************************
- *  Universidad Nacional Autónoma de México (UNAM)
- *  Facultad de Ingeniería | Departamento de Electrónica
- * 
- *  Asignatura:     Microprocesadores y Microcontroladores
- *  Profesor:       M.I. Christo Aldair Lara Tenorio
- *  Fecha:          15 de octubre de 2025
- * 
- *  Tema 08:        Lenguaje C
- *  Código 27:      Estructuras de control: IF-ELSE
- *  Descripción:    Construcción de la estructura de control IF-ELSE en lenguaje C.
- *                  Selección entre dos bloques de instrucciones, a partir del cumplimiento de una
- *                  condición.
- * 
- *  Tarjeta de desarrollo:  EK-TM4C1294XL Evaluation board
- ***********************************************/
-
-
-/*********************************************************************************
- *  Archivos de cabecera
- */
-
-#include <stdint.h>                                                                                 /*  Tipos enteros con tamaños fijos */
-
-
-/*********************************************************************************
- *  Definiciones y macros
+/** -----------------------------------------------------------------------------------------------
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ *
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       15 de octubre de 2025
+ *
+ * Tema 08:     Lenguaje C
+ * Código 26:   Estructuras de control: IF-ELSE
+ * Descripción: Construcción de la estructura de control IF-ELSE en lenguaje C.
+ *              Selección entre dos bloques de instrucciones, a partir del cumplimiento de una
+ *              condición.
+ *
+ * Tarjeta de desarrollo:       EK-TM4C1294XL Evaluation board
  */
 
 
-/*********************************************************************************
- *  Funciones
+/** -----------------------------------------------------------------------------------------------
+ * Archivos de cabecera
+ */
+
+#include <stdint.h>                                                                                 /* Tipos enteros con tamaños fijos */
+
+
+/** -----------------------------------------------------------------------------------------------
+ * Definiciones y macros
  */
 
 
-/*********************************************************************************
- *  Función principal
+/** -----------------------------------------------------------------------------------------------
+ * Funciones
+ */
+
+
+/** -----------------------------------------------------------------------------------------------
+ * Función principal
  */
 
 int main(void) {
 
-    int i = 0;
-    int j = 0;
+    int32_t i = 0;
+    int32_t j = 0;
 
-    int x = 5;                                                                                      /*  Variable de prueba (condición) */
+    int32_t x = 5;                                                                                  /* Variable de prueba (condición) */
 
     if (x == 5){
         /** Código del bloque IF. */
@@ -53,4 +53,7 @@ int main(void) {
         j++;
         j++;
     }
+
+    while (1) {}
+
 }
