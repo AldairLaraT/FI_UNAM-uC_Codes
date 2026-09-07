de1d6fb7b89c88e3a2d3d2575a70fa8f87d34c1b
Aldair Lara <aldairlarat@gmail.com>
2026-09-07 01:05:27 -0600

    feat: update to 2027-1 version

--- Workspace/c34-UserSWs_Debounce/main.c
+++ Workspace/c34-UserSWs_Debounce/main.c
@@ -123,7 +123,7 @@ void SysTick_Init_OneShot(uint32_t SysTick_Reload) {
     NVIC_ST_CTRL_R |= 0x01;                                                                         /* Enables SysTick to operate in a multi-shot way (PIOSC divided by 4) */
 
     /** Limpiar el valor de carga/inicio del SysTick para deshabilitar el contador en el siguiente
-     * ciclo. */
+     *  ciclo. */
     NVIC_ST_RELOAD_R = 0;                                                                           /* RELOAD: Reload Value -> Cleared */
 
 }
