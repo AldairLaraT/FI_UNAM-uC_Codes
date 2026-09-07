8add3143aa89dcec00a142f27f42de129f91df14
Aldair Lara <aldairlarat@gmail.com>
2026-09-07 00:34:32 -0600

    feat: update to 2027-1 version

--- Workspace/c29-EstructurasControl_DO_WHILE/main.c
+++ Workspace/c29-EstructurasControl_DO_WHILE/main.c
@@ -1,51 +1,56 @@
-/**************************************************************************************************
- *  Universidad Nacional Autónoma de México (UNAM)
- *  Facultad de Ingeniería | Departamento de Electrónica
- * 
- *  Asignatura:     Microprocesadores y Microcontroladores
- *  Profesor:       M.I. Christo Aldair Lara Tenorio
- *  Fecha:          15 de octubre de 2025
- * 
- *  Tema 08:        Lenguaje C
- *  Código 30:      Estructuras de control: DO-WHILE
- *  Descripción:    Construcción de la estructura de control DO-WHILE en lenguaje C.
- *                  Ejecución de un bloque de instrucciones al menos una vez y repetición si se
- *                  cumple una condición.
- * 
- *  Tarjeta de desarrollo:  EK-TM4C1294XL Evaluation board
- ***********************************************/
-
-
-/*********************************************************************************
- *  Archivos de cabecera
- */
-
-#include <stdint.h>                                                                                 /*  Tipos enteros con tamaños fijos */
-
-
-/*********************************************************************************
- *  Definiciones y macros
+/** -----------------------------------------------------------------------------------------------
+ * Universidad Nacional Autónoma de México (UNAM)
+ * Facultad de Ingeniería | Departamento de Electrónica
+ *
+ * Asignatura:  Microprocesadores y Microcontroladores
+ * Profesor:    M.I. Christo Aldair Lara Tenorio
+ * Fecha:       15 de octubre de 2025
+ *
+ * Tema 08:     Lenguaje C
+ * Código 29:   Estructuras de control: DO-WHILE
+ * Descripción: Construcción de la estructura de control DO-WHILE en lenguaje C.
+ *              Ejecución de un bloque de instrucciones al menos una vez y repetición si se
+ *              cumple una condición.
+ *
+ * Tarjeta de desarrollo:       EK-TM4C1294XL Evaluation board
  */
 
 
-/*********************************************************************************
- *  Funciones
+/** -----------------------------------------------------------------------------------------------
+ * Archivos de cabecera
+ */
+
+#include <stdint.h>                                                                                 /* Tipos enteros con tamaños fijos */
+
+
+/** -----------------------------------------------------------------------------------------------
+ * Definiciones y macros
  */
 
 
-/*********************************************************************************
- *  Función principal
+/** -----------------------------------------------------------------------------------------------
+ * Funciones
+ */
+
+
+/** -----------------------------------------------------------------------------------------------
+ * Función principal
  */
 
 int main(void) {
 
-    int i = 0;
+    int32_t i = 0;
 
-    int x = 5;                                                                                      /*  Variable de prueba (condición) */
+    int32_t x = 5;                                                                                  /* Variable de prueba (condición) */
 
     do {
+
         /** Código del bloque DO-WHILE. */
         i++;
         i++;
+
     } while (x == 5);
+
+    while (1) {}
+
 }
